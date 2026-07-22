08d34a85d7436c6c48a59e24345d2c7bc003babc
Rico-proxy <richardoahimire@gmail.com>
2026-07-22 05:54:38 +0100

    fix:added vercel.json to stop vercel's error

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { Toaster } from '@/components/ui/sonner'
 import { AccessibilityProvider } from '@/components/shared/AccessibilityProvider'
-
+import { Analytics } from "@vercel/analytics/next"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AccessibilityProvider>
       <App />
+      <Analytics />
       <Toaster />
     </AccessibilityProvider>
   </StrictMode>,
